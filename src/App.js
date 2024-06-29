3205b8425469c8dc619e6c6fbb65153f71500bc4
EsraaNasr92 <esraanasr706@gmail.com>
2024-06-29 16:00:42 +0300

    React-ToDoList

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const data = [
+    {
+      id: 1,
+      title: 'HTML',
+      note: 'Hyper Markup Language'
+    },
+    {
+      id: 2,
+      title: 'CSS',
+      note: 'Stylesheet'
+    },
+    {
+      id: 3,
+      title: 'JavaScript',
+      note: 'Scripting Lanaguage'
+    },
+    {
+      id: 4,
+      title: 'ReactJS',
+      note: 'JavaScript Framework'
+    },
+  ];
+
+  const [notes, setNotes] = useState(data);
+  const [title, setTitle] = useState('');
+  const [note, setNote] = useState('');
+  const [count, setCount] = useState(4);
+
+  // To add new Note
+  function addNote(){
+    setNotes([...notes, {id: count, title: title, note: note}]);
+    setCount(count + 1);
+    setTitle("");
+    setNote("");
+    console.log(notes);
+  }
+  //To remove Note
+  function removeItem(id) {
+    setNotes(notes.filter((e) => e.id !== id));
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="app">
+      <div className='header'>
+        <h1>ToDo</h1>
+        <input
+          type="text"
+          placeholder="add title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <button
+          type="submit"
+          onClick={addNote}
         >
-          Learn React
-        </a>
-      </header>
+          Submit
+        </button>
+      </div>
+        {notes.map((e) => (
+          <div className='notes' key={e.id}>
+            <h5>Title: {e.title}</h5>
+            <button
+              type="input"
+              onClick={() => removeItem(e.id)}
+            >
+              X
+            </button>
+          </div>
+        ))}
     </div>
   );
 }
