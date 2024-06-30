b3daa5901633dc8ac7405a6f60d3d32948cd0366
EsraaNasr92 <esraanasr706@gmail.com>
2024-06-30 13:15:47 +0300

    Add Counter

--- src/App.js
+++ src/App.js
@@ -3,40 +3,25 @@ import './App.css';
 
 function App() {
   const data = [
-    {
-      id: 1,
-      note: 'Hyper Markup Language'
-    },
-    {
-      id: 2,
-      note: 'Jog arounf the park 3x'
-    },
-    {
-      id: 3,
-      note: 'Scripting Lanaguage'
-    },
-    {
-      id: 4,
-      note: 'JavaScript Framework'
-    },
+    { id: 1, note: 'Hyper Markup Language' },
+    { id: 2, note: 'Jog around the park 3x' },
+    { id: 3, note: 'Scripting Language' },
+    { id: 4, note: 'JavaScript Framework' },
   ];
 
   const [notes, setNotes] = useState(data);
-  const [title, setTitle] = useState('');
-  const [note, setNote] = useState('');
   const [count, setCount] = useState(4);
+  const [note, setNote] = useState('');
 
   // To add new Note
-  function addNote(){
-    setNotes([...notes, {id: count,  note: note}]);
-    setCount(count + 1);
-    setNote("");
-    console.log(notes);
+  function addNote() {
+    if (note.trim() !== '') {
+      setNotes([...notes, { id: count + 1, note: note }]);
+      setCount(count + 1);
+      setNote('');
+    }
   }
-  //To remove Note
-  function removeItem(id) {
-    setNotes(notes.filter((e) => e.id !== id));
-  }
+  
   return (
     <div className="app">
       <div className='container'>
@@ -45,33 +30,31 @@ function App() {
           <input
             type="text"
             placeholder="Create a new todo..."
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            onKeyUp={(e) => (e.target.value)}
+            value={note}
+            onChange={(e) => setNote(e.target.value)}
+            onKeyUp={(e) => {
+              if (e.key === 'Enter') {
+                addNote();
+              }
+            }}
           />
         </div>
       </div>
       <div className='notes-container'>
         {notes.map((e) => (
-            <div className='notes' key={e.id}>
-              <h5>{e.note}</h5>
-{/*               <button
-                type="input"
-                onClick={() => removeItem(e.id)}
-              >
-                X
-              </button> */}
-            </div>
-          ))}
-          <div className='footer'>
-            <div className='counter'>items left</div>
-            <div className='footer-menu'>
-                  <a>All</a>
-                  <a>Active</a>
-                  <a>Completed</a>
-            </div>
-            <div className='clear'>Clear Completed</div>
+          <div className='notes' key={e.id}>
+            <h5>{e.note}</h5>
           </div>
+        ))}
+        <div className='footer'>
+          <div className='counter'>{count} items left</div>
+          <div className='footer-menu'>
+            <a>All</a>
+            <a>Active</a>
+            <a>Completed</a>
+          </div>
+          <div className='clear'>Clear Completed</div>
+        </div>
       </div>
     </div>
   );
